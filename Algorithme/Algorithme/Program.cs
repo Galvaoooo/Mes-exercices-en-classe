9bdba64907b5bd4a3dbc86f46632df3eea7dcc97
Galvaoooo <91313024+Galvaoooo@users.noreply.github.com>
2021-10-21 14:53:05 +0200

    Algorithme Ricardo no zip

---
+++ Algorithme/Algorithme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Algorithme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            string category = "";
+            Console.WriteLine("Veux-tu entrer ton age.");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age <= 5 || age > 14)
+            {
+                Console.WriteLine("Tu n'appartient a aucune catégorie");
+            }
+            else if (age <= 7)
+            {
+                Console.WriteLine("T'appartient à la categorie Poussin");
+            }
+            else if (age <= 9)
+            {
+                Console.WriteLine("T'appartient à la categorie Pupille");
+            }
+            else if (age <= 11)
+            {
+                Console.WriteLine("T'appartient à la categorie Minime");
+            }
+            else
+            {
+                Console.WriteLine("T'appartient a la categorie Cadet");
+            }
+        }
+    }
+}
+   
