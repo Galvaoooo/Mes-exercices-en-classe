2947ab0ce513af2dcf4c9de1207f24e40c5a6e1b
Galvaoooo <91313024+Galvaoooo@users.noreply.github.com>
2021-10-21 14:52:10 +0200

    Table5 Ricardo no zip

---
+++ TableDe5/TableDe5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TableDe5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int nombre;
+			int resultat;
+			int n;
+
+			nombre = 0;
+			for (int n = 1; n <= 100; n + 1)
+			{
+				resultat = n * 5;
+				nombre = nombre + "," + resultat;
+			}
+				Console.WriteLine(nombre));
+		}
+
+	}
+}
